671a1307deb32cc0e0c28c00fbbd982fa3e423ba
skidyuk8615 <chyu23072@g.nihon-u.ac.jp>
2024-10-21 11:20:35 +0900

    クリックした画像の表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from "react";
 
 export default function App() {
-
   const pic1 = {
     src: "images/pic1.jpg",
     alt: "Closeup of a human eye"
   };
-
   const pic2 = {
     src: "images/pic2.jpg",
     alt: "Rock that looks like a wave"
   };
-
   const pic3 = {
     src: "images/pic3.jpg",
     alt: "Purple and white pansies"
   };
-
   const pic4 = {
     src: "images/pic4.jpg",
     alt: "Section of wall from a pharoah's tomb"
   };
-
   const pic5 = {
     src: "images/pic5.jpg",
-    alt: "	Large moth on a leaf"
+    alt: "Large moth on a leaf"
   };
-
   const pics = [pic1, pic2, pic3, pic4, pic5];
+
+  const [displayedSrc, setDisplayedSrc] = useState("images/pic1.jpg");
+  const [displayedAlt, setDisplayedAlt] = useState("Closeup of a human eye");
+  function handleClick(pic) {
+    setDisplayedSrc(pic.src)
+    setDisplayedAlt(pic.alt)
+  };
 
   return (
     <>
@@ -35,8 +36,8 @@ export default function App() {
       <div className="full-img">
         <img
           className="displayed-img"
-          src="images/pic1.jpg"
-          alt="Closeup of a human eye"
+          src={displayedSrc}
+          alt={displayedAlt}
         />
         <div className="overlay"></div>
         <button className="dark">Darken</button>
@@ -45,7 +46,12 @@ export default function App() {
       <div className="thumb-bar">
         {pics.map((pic) => {
           return (
-            <img key={pic.src} src={pic.src} alt={pic.alt} />
+            <img
+              key={pic.src}
+              src={pic.src}
+              alt={pic.alt}
+              onClick={() => handleClick(pic)}
+            />
           );
         })}
       </div>
